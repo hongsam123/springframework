14c45f5f8e6699cee9339a116b234cf09177f751
eunsol <a2670624@naver.com>
2021-08-26 16:53:03 +0900

    최초 프로젝트 공유 파일

---
+++ src/main/java/com/mycompany/webapp/Ch01Controller.java
@@ -0,0 +1,18 @@
+package com.mycompany.webapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ch01")
+public class Ch01Controller {
+	private static final Logger logger = LoggerFactory.getLogger(Ch01Controller.class);
+
+	@RequestMapping("/content") // http://localhost:8080 가 앞에 생략 이후 + "/ch01/home"로 검색
+	public String home() {
+		logger.info("실행");
+		return "ch01/content"; // 뷰의 파일명
+	}	
+}
